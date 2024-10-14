daf1c4fbe48bcbcff0af478113e4a06ed354d42b
Eduardo Alves da Silva <eas@univali.br>
2024-10-14 12:29:10 +0200

    :zap: Update env settings

--- cog_challenge_server/setenv.sh
+++ cog_challenge_server/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # File env vars / application requirements
-export COG_FILESIZE_LIMIT=16 # Bytes
+export COG_FILESIZE_LIMIT=51200 # In Bytes (51.200 B == 50Kb)
 export COG_FILENAME_PREFIX="EduardoChallenge"
 
 # Server env vars / server setup info
